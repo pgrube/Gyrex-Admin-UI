dc681a864d66102b05b029ee9b5526ddf9dbc007
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-07-26 15:56:28 +0200

    make Navigation a composite
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

--- bundles/org.eclipse.gyrex.admin.ui/src/org/eclipse/gyrex/admin/ui/internal/application/Navigation.java
+++ bundles/org.eclipse.gyrex.admin.ui/src/org/eclipse/gyrex/admin/ui/internal/application/Navigation.java
@@ -23,36 +23,31 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
-public abstract class Navigation {
+public abstract class Navigation extends Composite {
 
-	private final Composite composite;
 	private final List<CategoryContribution> categories;
 
 	public Navigation(final Composite parent) {
-		composite = new Composite(parent, SWT.NONE);
-		composite.setLayout(AdminUiUtil.createGridLayoutWithoutMargin(9, false));
-		composite.setData(WidgetUtil.CUSTOM_VARIANT, "navigation");
+		super(parent, SWT.NONE);
+		setLayout(AdminUiUtil.createGridLayoutWithoutMargin(5, false));
+		setData(WidgetUtil.CUSTOM_VARIANT, "navigation");
 
 		// get and sort categories
 		categories = AdminPageRegistry.getInstance().getCategories();
 		Collections.sort(categories);
 
 		// create UI
-		createNavigationControls(composite);
+		for (final CategoryContribution category : categories) {
+			createNavigationDropDown(category);
+		}
 	}
 
 	private void changeSelectedDropDownEntry(final PageContribution page, final DropDownNavigation navEntry) {
 		navEntry.setSelected(pageBelongsToDropDownNav(page, navEntry));
 	}
 
-	private void createNavigationControls(final Composite parent) {
-		for (final CategoryContribution category : categories) {
-			createNavigationDropDown(parent, category);
-		}
-	}
-
-	private void createNavigationDropDown(final Composite parent, final CategoryContribution category) {
-		new DropDownNavigation(parent, category) {
+	private void createNavigationDropDown(final CategoryContribution category) {
+		new DropDownNavigation(this, category) {
 			@Override
 			protected void openPage(final PageContribution page) {
 				Navigation.this.openPage(page);
@@ -61,17 +56,13 @@ public abstract class Navigation {
 	}
 
 	public PageContribution findInitialPage() {
-		final Control[] children = composite.getChildren();
+		final Control[] children = getChildren();
 		for (final Control control : children) {
 			if (control instanceof DropDownNavigation) {
 				return ((DropDownNavigation) control).findFirstPage();
 			}
 		}
 		return null;
-	}
-
-	public Control getControl() {
-		return composite;
 	}
 
 	protected abstract void openPage(PageContribution page);
@@ -82,7 +73,7 @@ public abstract class Navigation {
 	}
 
 	public void selectNavigationEntry(final PageContribution page) {
-		final Control[] children = composite.getChildren();
+		final Control[] children = getChildren();
 		for (final Control control : children) {
 			if (control instanceof DropDownNavigation) {
 				changeSelectedDropDownEntry(page, (DropDownNavigation) control);
