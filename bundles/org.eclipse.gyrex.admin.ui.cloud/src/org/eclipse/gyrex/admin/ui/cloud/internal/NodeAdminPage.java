e636282cfb569b8e22c08780323f957e87f40879
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2012-08-21 20:56:57 +0200

    new page for nodes
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>

---
+++ bundles/org.eclipse.gyrex.admin.ui.cloud/src/org/eclipse/gyrex/admin/ui/cloud/internal/NodeAdminPage.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2011, 2012 Gunnar Wagenknecht and others.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gunnar Wagenknecht - initial API and implementation
+ */
+package org.eclipse.gyrex.admin.ui.cloud.internal;
+
+import org.eclipse.gyrex.admin.ui.pages.AdminPage;
+
+public class NodeAdminPage extends AdminPage {
+
+	public NodeAdminPage() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
